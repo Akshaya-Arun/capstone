9ff35aa00692a0ab5337cf607ee9fd380903d87e
Ravi Kumar <rav7179@gmail.com>
2023-08-23 19:17:01 +0530

    removed extra files

--- src/Pagination.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Pagination({goNext,goPrev}) {
-  return (
-    <div>
-        
-    </div>
-  )
-}
--- src/Pokemon.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Pokemon({pokemon}) {
-  return (
-    <div>
-        {pokemon.map(p=><p>{p}</p>)}
-    </div>
-  )
-}
